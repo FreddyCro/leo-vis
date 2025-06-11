ed1d5d16ee3a3ed831dbd0827455424e61e30f2e
FreddyCro <freddy10100@hotmail.com>
2025-06-11 11:15:47 +0800

    feat(bar chart): added bar chart component and integrate into section hero

--- src/components/section-hero.vue
+++ src/components/section-hero.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import Leo3dEarthHero from '@/components/leo-3d-earth-hero.vue';
+import LeoBarChart from '@/components/leo-bar-chart.vue';
 import LeoScrollTrigger from '@/components/leo-scroll-trigger.vue';
 import LeoSectionIntro from '@/components/leo-section-intro.vue';
 import LeoSectionLayout from '@/components/leo-section-layout.vue';
@@ -9,6 +10,24 @@ import str from '@/locales/section-hero.json';
 
 const isZoomOut = ref(false);
 const isBarChartTriggerIntersecting = ref(false);
+const BAR_CHART_DATA1 = [
+  { label: '商業太空使用', data: [330, 525, 755], backgroundColor: '#00f7ef' },
+  { label: '進行太空運用', data: [300, 635, 1035], backgroundColor: '#008b85' },
+];
+const BAR_CHART_DATA2 = [
+  {
+    label: '商業太空使用',
+    data: [630, 1160, 1790],
+    backgroundColor: '#00f7ef',
+  },
+];
+const BAR_CHART_LABELS = ['2023年', '2030年', '2035年'];
+
+const barChartData = computed(() => {
+  return isBarChartTriggerIntersecting.value
+    ? BAR_CHART_DATA2
+    : BAR_CHART_DATA1;
+});
 
 function onZoomOutIntersectChange(isIntersecting: boolean) {
   isZoomOut.value = isIntersecting;
@@ -64,11 +83,19 @@ function onBarChartIntersectChange(isIntersecting: boolean) {
                 </figcaption>
 
                 <div class="bg-red-300 p-10">
-                  {{
-                    isBarChartTriggerIntersecting
-                      ? 'Bar chart B'
-                      : 'Bar chart A'
-                  }}
+                  <LeoBarChart
+                    :data="{
+                      labels: BAR_CHART_LABELS,
+                      datasets: barChartData,
+                    }"
+                    :options="{
+                      width: 320,
+                      height: 120,
+                      barHeight: 24,
+                      barGap: 18,
+                      labelWidth: 48,
+                    }"
+                  />
                 </div>
 
                 <figcaption>
