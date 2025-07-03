16cd7817431d197a1e0184307ae7029a7bc400ee
FreddyCro <freddy10100@hotmail.com>
2025-07-03 13:31:29 +0800

    fix(intro): style

--- src/components/leo-section-intro.vue
+++ src/components/leo-section-intro.vue
@@ -129,7 +129,8 @@ function onClick(item: { text: string; link: string }) {
   }
 
   &__present-line-2 {
-    top: calc(var(--init-screen-height) - 67px);
+    /* top: calc(var(--init-screen-height) - 67px); */
+    top: calc(var(--init-screen-height) - 80px);
     width: 100%;
     height: 1px;
 
